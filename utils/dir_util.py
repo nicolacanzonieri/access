acd1059576ecb5414cfe3ac95726864a6c7db4d1
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-06-08 20:28:31 +0200

    Small changes to utils/dir_util.py

--- utils/dir_util.py
+++ utils/dir_util.py
@@ -12,10 +12,10 @@ Return the os' accepted path (str) to a specified file with the correct path sep
                         path_to_file = "folder1 folder2 folder3 ... file"
 '''
 def get_path_to(path_to_file) -> str:
-    path_array = path_to_file.split()
+    path_vec = path_to_file.split()
     full_path = ""
 
-    for item in path_array:
+    for item in path_vec:
         full_path = full_path + get_path_separator() + item
     full_path = "." + get_path_separator() + full_path
     
