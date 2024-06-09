01ddeaebe0bc6f5a74ad340b95e38680e820e48b
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-06-09 11:13:50 +0200

    Better functions specifications in vec_util

--- utils/vec_util.py
+++ utils/vec_util.py
@@ -1,7 +1,16 @@
+'''
+Print a python list in terminal
+@param "vec" : a python list
+'''
 def print_vec(vec):
     for item in vec:
         print(item)
 
+
+'''
+Print a python matrix in terminal
+@param "matrix" : a python matrix
+'''
 def print_matrix(matrix):
     for row in matrix:
         for item in row:
