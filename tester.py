e10adc18d3c084960ee4ae9cff48f008995ac5ce
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-06-09 10:33:22 +0200

    Added clean_str test to tester

--- tester.py
+++ tester.py
@@ -2,10 +2,15 @@
 
 from utils.dir_util import get_path_to
 from utils.json_util import json_to_vec
+from utils.str_util import clean_str
 
 def test_json_reader():
     path_to_json = get_path_to("json example.json")
-    json_to_vec(path_to_json)
+    json_to_vec(path_to_json, "nome eta citta amici")
+
+def test_clean_str():
+    string = "   Mario Rossi"
+    print(clean_str(string))
 
 def main():
     test_json_reader()
