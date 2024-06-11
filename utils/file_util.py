87315b84cc7e49354b17b4c1cebede7b3052579c
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-06-11 12:29:53 +0200

    Better file_util.py

--- utils/file_util.py
+++ utils/file_util.py
@@ -7,11 +7,20 @@ Index:
 
 
 '''
-Print a file on a specified path, on terminal
+Print a file located on a specified path, on terminal
 @param "path_to_file" : a string containing the path to a file
 '''
 def print_file(path_to_file):
     with open(path_to_file, "r") as file:
         file_lines = file.read()
         print(file_lines)
-    # TODO!
+
+
+'''
+Return a string containing a text file located on a specified path
+@param "path_to_file" : a string containing the path to a file
+'''
+def get_file(path_to_file) -> str:
+    with open(path_to_file, "r") as file:
+        file_lines = file.read()
+        return file_lines
