2abb15f745f8120a2b98ee904183a2d063c8585c
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-07-04 13:42:15 +0000

    Added find_value_in_vec in vec_util

--- utils/vec_util.py
+++ utils/vec_util.py
@@ -4,6 +4,7 @@ VEC UTIL
 Index:
 - print_vec()
 - print_matrix()
+- find_value_in_vec()
 '''
 
 
@@ -24,3 +25,24 @@ def print_matrix(matrix):
     for row in matrix:
         for item in row:
             print(item, end=", ")
+
+
+'''
+Return the index where a given value is located inside a vec
+@param "val" : value to find
+@param "vec" : list where to search the value
+@return "index" : the position where val is located. If val is not in vec than index = -1
+'''
+def find_value_in_vec(val, vec) -> int:
+    index = 0
+    while index < len(vec):
+        try {
+        	if vec[index] == val:
+                return index
+        } except {
+        	print("FIND_VALUE_IN_VEC")
+            print("Can't compare given val with vec values")
+            return -1
+        }
+        index += 1
+    return -1
