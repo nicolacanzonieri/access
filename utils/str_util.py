2f20e624f65d259245bc2deed816332d08869068
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-07-05 08:15:18 +0000

    Moved str_to_int from var_util to str_util

--- utils/str_util.py
+++ utils/str_util.py
@@ -5,7 +5,11 @@ Index:
 - clean_str()
 - str_to_char_list()
 - str_to_vec()
+- str_to_int()
 '''
+
+
+import math
 
 
 '''
@@ -66,3 +70,15 @@ def str_to_vec(string) -> list:
             sub_file_line = ""
         char_index += 1
     return str_vec
+
+
+'''
+Returns the result of converting a string to an integer.
+@param string: A string containing only numbers.
+'''
+def str_to_int(string) -> int:
+    char = string[0]
+    if len(string) > 1:
+        return (ord(char) - 48) * pow(10, (len(string) - 1)) + str_to_int(string[1:])
+    else:
+        return (ord(char) - 48)
