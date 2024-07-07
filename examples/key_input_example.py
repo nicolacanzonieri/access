59a10531972907ce7796262f98341bc21e5ee247
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-07-07 23:30:26 +0200

    Better key_input_example

--- examples/key_input_example.py
+++ examples/key_input_example.py
@@ -1,6 +1,24 @@
 import sys
 import os
 
+
+banned_keys = []
+def detect_banned_key(key):
+    global banned_keys
+    if ord(key) == 224 and len(banned_keys) == 0:
+        banned_keys.append(ord(key))
+        print("CHECK 1")
+        return None
+    elif (ord(key) == 72 or ord(key) == 77 or ord(key) == 80 or ord(key) == 75) and len(banned_keys) == 1:
+        print("CHECK 2 - POSITIVE")
+        banned_keys = []
+        return True
+    else:
+        print("CHECK 2 - NEGATIVE")
+        banned_keys = []
+        return False
+
+
 '''
 Detect special keys pressed by the user
 '''
@@ -10,27 +28,31 @@ if sys.platform == "win32":
     def get_key():
         while True:
             key = msvcrt.getch()
-            print(key)
-            print(ord(key))
-            if key == b'\t':      # Up
+            print(type(key), end=" ~ ")
+            print(ord(key), end=" ~ ")
+            print(key, end=" ~ ")
+            print(chr(ord(key)))
+            key_check = detect_banned_key(key)
+            if key == b'\t' and key_check == False:      # Up
                 return "CTRL+i"
-            elif key == b'\x0c':  # Right
+            elif key == b'\x0c' and key_check == False:  # Right
                 return "CTRL+l"
-            elif key == b'\x0b':  # Down
+            elif key == b'\x0b' and key_check == False:  # Down
                 return "CTRL+k"
-            elif key == b'\n':    # Left
+            elif key == b'\n' and key_check == False:    # Left
                 return "CTRL+j"
-            elif key == b'\x0f':  # Fast right
+            elif key == b'\x0f' and key_check == False:  # Fast right
                 return "CTRL+o"
-            elif key == b'\x15':  # Fast left
+            elif key == b'\x15' and key_check == False:  # Fast left
                 return "CTRL+u"
-            elif key == b'\x17':  # Close
+            elif key == b'\x17' and key_check == False:  # Close
                 return "CTRL+w"
-            elif key == b"\x08":  # Backspace/Delete key
+            elif key == b"\x08" and key_check == False:  # Backspace/Delete key
                 return "DELETE"
-            elif key == b"\xe0":
+            elif key == b"\xe0" and key_check == False:
                 return "CANCEL"
-            return key.decode("utf-8")
+            elif key_check == False:
+                return key.decode("utf-8")
 
 else:
     import tty
