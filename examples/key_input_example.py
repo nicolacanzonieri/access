c1e8e33c6ce3f1ed31b72496f6571722947350e7
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2024-07-08 21:16:37 +0000

    Better key_input_example

--- examples/key_input_example.py
+++ examples/key_input_example.py
@@ -30,6 +30,45 @@ def detect_arrow_key(key):
 
 
 '''
+Print useful informations about the pressed key
+'''
+def key_debugger(key):
+    print(type(key), end=" ~ ")
+    print(ord(key), end=" ~ ")
+    print(key, end=" ~ ")
+    print(chr(ord(key)))
+
+
+'''
+Return the respective key/combination pressed by the user@
+@param "key" : native variable containing the pressed key
+@param "arrow_key" : container for the arrow key
+'''
+def key_identifier(key, arrow_key):
+	if ord(key) == 9 and arrow_key == None:      # Up
+		return "CTRL+i"
+	elif ord(key) == 12 and arrow_key == None:  # Right
+		return "CTRL+l"
+	elif ord(key) == 11 and arrow_key == None:  # Down
+		return "CTRL+k"
+	elif ord(key) == 10 and arrow_key == None:    # Left
+		return "CTRL+j"
+	elif ord(key) == 15 and arrow_key == None:  # Fast right
+		return "CTRL+o"
+	elif ord(key) == 21 and arrow_key == None:  # Fast left
+		return "CTRL+u"
+	elif ord(key) == 23 and arrow_key == None:  # Close
+		return "CTRL+w"
+	elif ord(key) == 127 and arrow_key == None:  # Backspace/Delete key
+		return "DELETE"
+	elif ord(key) == 126 and arrow_key == None:
+		return "CANCEL"
+	elif arrow_key != None:
+		return arrow_key
+	elif arrow_key == "":
+        return key.decode("utf-8")
+
+'''
 Detect special keys pressed by the user
 '''
 if sys.platform == "win32":
@@ -38,34 +77,9 @@ if sys.platform == "win32":
     def get_key():
         while True:
             key = msvcrt.getch()
-            print(type(key), end=" ~ ")
-            print(ord(key), end=" ~ ")
-            print(key, end=" ~ ")
-            print(chr(ord(key)))
+            key_debugger(key)
             arrow_key = detect_arrow_key(key)
-            if key == b'\t' and arrow_key == None:      # Up
-                return "CTRL+i"
-            elif key == b'\x0c' and arrow_key == None:  # Right
-                return "CTRL+l"
-            elif key == b'\x0b' and arrow_key == None:  # Down
-                return "CTRL+k"
-            elif key == b'\n' and arrow_key == None:    # Left
-                return "CTRL+j"
-            elif key == b'\x0f' and arrow_key == None:  # Fast right
-                return "CTRL+o"
-            elif key == b'\x15' and arrow_key == None:  # Fast left
-                return "CTRL+u"
-            elif key == b'\x17' and arrow_key == None:  # Close
-                return "CTRL+w"
-            elif key == b"\x08" and arrow_key == None:  # Backspace/Delete key
-                return "DELETE"
-            elif key == b"\xe0" and arrow_key == None:
-                return "CANCEL"
-            elif arrow_key != None:
-                return arrow_key
-            elif arrow_key == "":
-                return key.decode("utf-8")
-
+            return key_identifer(key, arrow_key)
 else:
     import tty
     import termios
@@ -76,28 +90,13 @@ else:
         try:
             tty.setraw(fd)
             key = sys.stdin.read(1)
-            print(ord(key))
-            if ord(key) == 9:     # Up
-                return "CTRL+i"
-            elif ord(key) == 12:  # Right
-                return "CTRL+l"
-            elif ord(key) == 11:  # Down
-                return "CTRL+k"
-            elif ord(key) == 10:  # Left
-                return "CTRL+j"
-            elif ord(key) == 15:  # Fast right
-                return "CTRL+o"
-            elif ord(key) == 21:  # Fast left
-                return "CTRL+u"
-            elif ord(key) == 23:  # Close
-                return "CTRL+w"
-            elif ord(key) == 127:
-                return "DELETE"
-            elif ord(key) == 126:
-                return "CANCEL"
+            key_debugger(key)
+            arrow_key = detect_arrow_key(key)
+            return key_identifer(key, arrow_key)
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return key
+
 
 def start_key_input_example():
     while True:
